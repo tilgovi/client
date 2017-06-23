ea617c3aca1f3a1178e7ca79d193e7f1e8beadca
Sean Roberts <sean.roberts90@gmail.com>
2017-06-23 12:23:39 -0400

    Adding feature flag checking and decision for overlay-highlighter

---
+++ src/annotator/highlighter/overlay-highlighter/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  highlightRange: () => {
+    // eslint-disable-next-line no-console
+    console.log('highlightRange not implemented');
+  },
+
+  removeHighlights: () => {
+    // eslint-disable-next-line no-console
+    console.log('highlightRange not implemented');
+  },
+
+  getBoundingClientRect: () => {
+    // eslint-disable-next-line no-console
+    console.log('highlightRange not implemented');
+  },
+};
